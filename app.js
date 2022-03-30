b91c24f37c56ed9c375eb8b530a66075ff4a2885
austin <austinjbrock@gmail.com>
2022-03-29 19:25:56 -0700

    Project finished and deployed to

--- app.js
+++ app.js
@@ -3,31 +3,30 @@ const inputEl = document.getElementById('input-el')
 let myLeads = []
 const ulEl = document.getElementById('ul-el')
 const deleteBtn = document.getElementById('delete-btn')
+const saveTab = document.getElementById('save-tab')
 //Parses data into strings
-let leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"))
-
+const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"))
 
 //dynamically creats list items from user input
 
-const renderLeads = () =>{
+const render = (leads) =>{
     let listItems = '';
-    for(let i =0; i< myLeads.length; i ++){
+    for(let i =0; i< leads.length; i ++){
         listItems +=`
         <li>
-            <a target='_blank' href='${myLeads[i]}'>
-            ${ myLeads[i]} 
+            <a target='_blank' href='${leads[i]}'>
+            ${leads[i]} 
             </a>
         </li>`
     }
 ulEl.innerHTML = listItems;
 }
 
-
-
 //sets current leads based on local storage 
+
 if(leadsFromLocalStorage){
     myLeads = leadsFromLocalStorage
-    renderLeads()
+    render(myLeads)
 }
 
 //Grabs user input and renders to screen
@@ -35,14 +34,25 @@ inputBtn.addEventListener('click',() =>{
     myLeads.push(inputEl.value)
     inputEl.value = '';
     localStorage.setItem("myLeads", JSON.stringify(myLeads))
-    renderLeads()
+    render(myLeads)
 
 })
-
+  
 //Deletes list items and clears local storage
 
 deleteBtn.addEventListener('click',()=>{
     ulEl.innerHTML = ''
     localStorage.clear()
+ 
+})
 
+//Save Tab 
+
+saveTab.addEventListener('click', ()=>{
+    //grab current tab URL
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+        myLeads.push(tabs[0].url)
+        localStorage.setItem("myLeads", JSON.stringify(myLeads))
+        render(myLeads)
+     });   
 })
